a6612270bf3072e20ff0f88a32f9b53d43af1a37
maasa333 <konomaasa@gmail.com>
2020-02-07 14:37:55 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 // Your code here
+function mapToNegativize(array) {
+    const new_array = []
+    for (let i = 0; i < array.length; i++) {
+        new_array.push(array[i] * -1)
+    }
+    return new_array
+}
+
+function mapToNoChange(array) {
+    const new_array = []
+    for (let i = 0; i < array.length; i++) {
+        new_array.push(array[i])
+    }
+    return new_array
+}
+
+function mapToDouble(array) {
+    const new_array = []
+    for (let i = 0; i < array.length; i++) {
+        new_array.push(array[i] * 2)
+    }
+    return new_array
+}
+
+function mapToSquare(array) {
+    const new_array = []
+    for (let i = 0; i < array.length; i++) {
+        new_array.push(array[i] * array[i])
+    }
+    return new_array
+}
+
+function reduceToTotal(array, index=0) {
+    for (let i = 0; i < array.length; i++) {
+        index = array[i] + index
+    }
+    return index
+}
+
+function reduceToAllTrue(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (!!array[i] !== true) {
+            return false
+        }
+    }
+    return true
+}
+
+function reduceToAnyTrue(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (!!array[i] === true) {
+            return true
+        }
+    }
+    return false
+}
